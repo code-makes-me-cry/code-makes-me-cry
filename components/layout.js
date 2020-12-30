fcdb318ea5174caa58767312b01d4a44510143f7
Eva Larumbe <evalarumbe@gmail.com>
2020-12-30 10:48:39 +0100

    Install ESLint and Prettier

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-const name = 'Eva L'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Code Makes Me Cry';
+export const siteTitle = 'Code Makes Me Cry';
 
 export default function Layout({ children, home }) {
   return (
@@ -17,7 +17,9 @@ export default function Layout({ children, home }) {
         />
         <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -34,20 +36,20 @@ export default function Layout({ children, home }) {
           </>
         ) : (
           <>
-          <Link href="/">
-            <a>
-              <img
-                src="/images/profile.jpg"
-                className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                alt={name}
-              />
-            </a>
-          </Link>
-          <h2 className={utilStyles.headingLg}>
             <Link href="/">
-              <a className={utilStyles.colorInherit}>{name}</a>
+              <a>
+                <img
+                  src="/images/profile.jpg"
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
             </Link>
-          </h2>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
           </>
         )}
       </header>
@@ -60,5 +62,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  )
+  );
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,8 @@
-import Layout from '../../components/layout'
-import Head from 'next/head'
-import Date from '../../components/date'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import utilStyles from '../../styles/utils.module.css'
+import Layout from '../../components/layout';
+import Head from 'next/head';
+import Date from '../../components/date';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
   return (
@@ -21,26 +21,27 @@ export default function Post({ postData }) {
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  )
+  );
 }
 
 // Runs once. Calls getStaticProps (therefore also Post) for each item in paths
 export async function getStaticPaths() {
-  const paths = getAllPostIds() // array of context objects, each with a with params key
+  const paths = getAllPostIds(); // array of context objects, each with a with params key
 
   // return an array of possible values for [id]
   return {
     paths,
-    fallback: false // false: posts/non-existing-path will result in 404
-  }
+    fallback: false, // false: posts/non-existing-path will result in 404
+  };
 }
 
-export async function getStaticProps({ params }) { // params property from a context object
-  const postData = await getPostData(params.id)
+export async function getStaticProps({ params }) {
+  // params property from a context object
+  const postData = await getPostData(params.id);
 
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
